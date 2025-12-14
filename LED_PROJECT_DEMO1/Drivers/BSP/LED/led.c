15c36f6b33276f70cb16380e2d69a27659802384
jwd <jiawdgoo@gmail.com>
2025-12-14 19:49:52 +0800

    led

--- LED_PROJECT_DEMO1/Drivers/BSP/LED/led.c
+++ LED_PROJECT_DEMO1/Drivers/BSP/LED/led.c
@@ -8,9 +8,12 @@ void led_init(void)
 
     gpio_init_struct.Pin = LED0_GPIO_PIN;
     gpio_init_struct.Mode = GPIO_MODE_OUTPUT_PP;
-    gpio_init_struct.Pull = GPIO_PULLUP;
+    gpio_init_struct.Pull = GPIO_PULLUP;//默认上拉
     gpio_init_struct.Speed = GPIO_SPEED_FREQ_HIGH;
     HAL_GPIO_Init(LED0_GPIO_PORT, &gpio_init_struct);
+
+    gpio_init_struct.Pin = LED1_GPIO_PIN;
+    HAL_GPIO_Init(LED1_GPIO_PORT, &gpio_init_struct);
 
     LED0(1);
     LED1(1);
